d1888883625b6a185bf43f368a0b0f561a26d614
Mihai Chis <mh_chis@yahoo.com>
2018-12-13 12:29:56 +0200

    second commit second branch

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
 
 
         // chis
-
+        //Mihai
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
