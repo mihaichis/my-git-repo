db3ceb17f88da4af7e2d0c32c028873e7789b474
Mihai Chis <mh_chis@yahoo.com>
2018-12-13 12:28:10 +0200

    second commit first Mihai

--- MyApplication/app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.kt
+++ MyApplication/app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.kt
@@ -10,7 +10,7 @@ import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
- *
+ *hhjgj
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-
+// Mihai
 
 
         
